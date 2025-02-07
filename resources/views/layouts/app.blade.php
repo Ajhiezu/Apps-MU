281bbb6a6e72ba8ecf18136d75bb3ae619f262d5
elspam <kicaumerdu1@gmail.com>
2025-02-07 20:08:53 +0700

    heading

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,11 +1,28 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="id">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{config('app.name','laravel')}}</title>
+    <link rel="stylesheet" href="{{ asset ('asset/css/nav.css')}}">
 </head>
 <body>
-    
+    <nav>
+        <div class="logo">
+            <img src="asset\img\logo-appMu.svg" alt="logo" >
+            <h4 style="display: flex";>AppsMU</h4>
+        </div>
+        <ul class="text-menu">
+            <li><a href="home">Home</a></li>
+            <li><a href="contact">Contact</a></li>
+            <li><a href="shop">Shop</a></li>
+            <li><a href="about">About</a></li>
+        </ul>
+        <ul class="icon-menu">
+            <li><a href="home">Home</a></li>
+            <li><a href="contact">Contact</a></li>
+            <li><a href="shop">Shop</a></li>
+        </ul>
+    </nav>
 </body>
 </html>
